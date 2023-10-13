5a13cfa602dfcd1ff3a3ee2e111e180302d0aba2
Moon <100405650+chocobubble@users.noreply.github.com>
2023-10-14 00:37:25 +0900

    231014 data

--- Source/LooterShooter/LSGameInstance.h
+++ Source/LooterShooter/LSGameInstance.h
@@ -26,11 +26,7 @@ struct FLSWeaponBaseData : public FTableRowBase
 	GENERATED_BODY()
 
 public:
-	FLSWeaponBaseData() : Level(1), MagazineCapacity(20), FireRate(500.0f),
-		MovementSpeed(510.0f), BulletDamage(50.0f), CriticalHitChance(0.03f), 
-		CriticalHitMultiplier(1.5f), DamageReduceDistance(1000.0f), ReloadTime(2.0f),
-		BulletsPerCatridge(1), MaxRange(10000.0f), MaxSpreadAngle(1.0f),
-		MinSpreadAngle(0.0f), HeatPerShot(0.1f) {}
+	FLSWeaponBaseData() : 
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon)
 	int32 Level = 1;
@@ -81,36 +77,32 @@ struct FLSPlayerData : public FTableRowBase
 	GENERATED_BODY()
 
 public:
-	FLSPlayerData() : 
-		Level(1), MaxHP(100.0f), MaxShield(50.f), MaxMP(1.f), Defense(1.f),
-		ShieldRechargeRate(0.1f), ShieldRechargeDelay(3.f), NextExp(5), MasteryExp(1) {}
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	int32 Level = 1;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	int32 Level;
+	float MaxHP = 100.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	float MaxHP;
+	float MaxShield = 50.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	float MaxShield;
+	float MaxMP = 1.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	float MaxMP;
+	float Defense = 1.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	float Defense;
+	float ShieldRechargeRate = 0.1f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	float ShieldRechargeRate;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	float ShieldRechargeDelay;
+	float ShieldRechargeDelay = 3.0f;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	int32 NextExp;
+	int32 NextExp = 5;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	int32 MasteryExp;
+	int32 MasteryExp = 5;
 };
 
 USTRUCT(BlueprintType)
@@ -119,82 +111,27 @@ struct FLSMonsterData : public FTableRowBase
 	GENERATED_BODY()
 
 public:
-	FLSMonsterData() : 
-		Level(1), MaxHP(100.0f), MaxShield(50.f), Defense(1.f),
-		ShieldRechargeRate(0.1f), ShieldRechargeDelay(3.f), DropExp(5) {}
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	int32 Level = 1;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	int32 Level;
+	float MaxHP = 100.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	float MaxHP;
+	float MaxShield = 50.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	float MaxShield;
+	float Defense = 1.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	float Defense;
+	float ShieldRechargeRate = 0.1f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	float ShieldRechargeRate;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	float ShieldRechargeDelay;
+	float ShieldRechargeDelay = 3.0f;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
-	int32 DropExp;
+	int32 DropExp = 5;
 };
-/*
-USTRUCT(BlueprintType)
-struct FLSWeaponBaseData : public FTableRowBase
-{
-	GENERATED_BODY()
-
-public:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon)
-	int32 Level;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon)
-	int32 MagazineCapacity = 20;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon)
-	float FireRate = 500.f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon)
-	float MovementSpeed = 510.f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon)
-	float BulletDamage = 50.f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon)
-	float CriticalHitChance = 0.03f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon)
-	float CriticalHitMultiplier = 1.5f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon)
-	float DamageReduceDistance = 1000.f;
-
-	UPROPERTY(EditAnywhere,  BlueprintReadWrite, Category = Weapon)
-	float ReloadTime = 2.f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon)
-	int32 BulletsPerCatridge = 1;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Weapon)
-	float MaxRange = 10000.f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Attack)
-	float MaxSpreadAngle  = 1.f;
-
-	UPROPERTY(EditAnywhere,  BlueprintReadWrite, Category = Attack)
-	float MinSpreadAngle = 1.f;
-
-	UPROPERTY(EditAnywhere,  BlueprintReadWrite, Category = Attack)
-	float HeatPerShot = 0.1f;
-};
-
-*/
 
 /**
  * 
@@ -211,11 +148,7 @@ public:
 	FLSPlayerData* GetLSPlayerData(int32 Level);
 	FLSMonsterData* GetLSMonsterData(int32 Level);
 	FLSWeaponBaseData* GetWeaponData(EWeaponType WeaponType, int32 ItemLevel);
-/*
-	FLSWeaponBaseData* GetLSRifleData(int32 ItemLevel);
-	FLSWeaponBaseData* GetLSShotgunData(int32 ItemLevel);
-	FLSWeaponBaseData* GetLSPistolData(int32 ItemLevel);
-*/
+
 	USkeletalMesh* GetRifleMesh() const
 	{
 		return RifleWeaponMesh;
